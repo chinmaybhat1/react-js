5b6caca9ec0e79ed8c5c1405c7641105878cc810
Chinmay Bhat <chinmay.bhat@cerner.com>
2019-09-24 22:26:49 +0530

    Introduction to class components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Functional from './Components/Functional';
+import ClassComponent from './Components/ClassComponent';
 
 function App() {
   return (
     <div className="App">
       <Functional/>
+      <ClassComponent/>
     </div>
   );
 }
---
+++ src/Components/ClassComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class ClassComponent extends Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome to React JS</p>
+      </div>
+    );
+  }
+}
+
+export default ClassComponent;
